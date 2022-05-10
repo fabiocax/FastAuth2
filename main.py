7682af21c2e84a16fa4f7c0069c5344510f3b5af
Fabio Alberti <fabiocax@gmail.com>
2022-05-10 18:03:09 -0300

    teste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI 
+from fastapi.openapi.utils import get_openapi
 from oauth import main as auth
 import uvicorn
 from cli import Calculator
@@ -7,6 +8,25 @@ from cli import Calculator
 
 app = FastAPI()
 app.include_router(auth.oauth)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="FastAuth2",
+        version="2.5.0",
+        description="",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 @app.get("/")
 async def root():
